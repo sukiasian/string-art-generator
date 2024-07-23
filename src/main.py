3680c7b6462b3de4647a82617addd5f4206870a6
Sam <63611463+sukiasian@users.noreply.github.com>
2024-07-23 07:17:39 -0700

    Implement lowest residual algorithm

--- src/main.py
+++ src/main.py
@@ -1,36 +1,14 @@
-# from dotenv import load_dotenv
-# from cv2 import imread
-# from string_generator import String_Generator
-# from image_processors.grayscaler import Grayscale
-# from image_processors.enhancer import Enhancer
+from PIL import Image, ImageDraw
+import numpy as np
 
-# load_dotenv()
+a = 'src/assets/initial.jpg'
+b = 'src/assets/c.jpg'
 
+im_1 = Image.open(a)
 
-# # Grayscale('src/assets/cropped.png').process()
-# source_image = imread('src/assets/cropped.png')
-# bitmap = source_image
+grayscale = im_1.convert('L')
 
-# strGenerator = String_Generator(source_image=source_image, pins=240, iterations=3000, radius=32)
+grayscale_image_matrix =  np.array(grayscale)
 
-# strGenerator.generate()
-
-
-# from cv2 import imread
-# from PIL import Image, ImageDraw
-# import numpy as np
-
-# a = 'src/assets/initial.jpg'
-# b = 'src/assets/c.jpg'
-
-# im_1 = Image.open(a)
-
-# grayscale = im_1.convert('L')
-# print(grayscale.size)
-
-
-# grayscale_image_matrix =  np.array(grayscale)
-
-# print(len(grayscale_matrix[0]))
 
 import generator
--- src/string_generator.py
+++ src/string_generator.py
@@ -1,181 +1,175 @@
-from bresenham import bresenham
-from random import randint
+import numpy as np
 import math
+from PIL import Image
+from random import randint
+from bresenham import bresenham
 
-class String_Generator: 
-	pins_coordinates = []
-	output = []
 
-	def __init__(self, pins, radius, source_image, iterations = 3000): 
-		self.pins = pins
-		self.radius = radius
-		self.source_image = source_image
-		self.iterations = iterations
+image = Image.open('src/assets/cropped.png').convert('L').resize((100,  100))
 
-		self.angle_between_pins = pins / 360
+image_matrix = np.array(image)
 
-		self.__set_pins_coordinates()
+sample_factor = 4
+supersampling = image_matrix.shape[0] * sample_factor
+
+image_negative = 1 - image_matrix
+image_matrix_normalized = image_negative / 255
+
+def get_supersampled_canvas():
+  return np.zeros((supersampling, supersampling))
+
+
+def downsample(matrix):
+  rows_downsampled = matrix.shape[0] // sample_factor
+  cols_downsampled = matrix.shape[1] // sample_factor
+
+  blocks = matrix.reshape(rows_downsampled, sample_factor, cols_downsampled, sample_factor)
+
+  matrix_downsampled = np.mean(blocks, axis=(1, 3))
+
+  return matrix_downsampled
+
+pins_amount = 240
+angle_between_pins = 360 / pins_amount
+radius_supersampled = supersampling / 2
+
+pins_coordinates = np.empty((240, 2))
+
+
+def to_radian(angle):
+	return (math.pi * angle) / 180
+
+
+def get_x_0(angles):
+	return radius_supersampled * np.cos(angles)
+
+
+def get_y_0(angles):
+	return radius_supersampled * np.sin(angles)
+
+
+def set_pins_coordinates():
+	angles = np.arange(240) * angle_between_pins
+	angles_radians = np.radians(angles)
+
+	pins_coordinates[:, 0] = get_x_0(angles_radians) + radius_supersampled
+	pins_coordinates[:, 1] = get_y_0(angles_radians) + radius_supersampled
+
+
+set_pins_coordinates()
+
+bresenham_cache = {}
+
+def get_bresenham(i):
+  x0, y0 = pins_coordinates[latest_pin]
+  x1, y1 = pins_coordinates[i]
+
+  x0 = math.floor(x0) - 1
+  y0 = math.floor(y0) - 1
+  x1 = math.floor(x1) - 1
+  y1 = math.floor(y1) - 1
+
+  key = tuple(sorted([(x0, y0), (x1, y1)]))
+
+  if key not in bresenham_cache:
+        bresenham_cache[key] = np.array(list(bresenham(x0, y0, x1, y1)))
+
+  return bresenham_cache[key]
+
+
+# Number of edges
+strings_number = 0
+
+# Tracking the pins posiotions
+initial_pin = randint(5, 50)
+latest_pin = initial_pin
+
+
+# writing the results of pins at iterations to have the sequence then
+with open('output.txt', 'a') as f:
+	f.write(f'{latest_pin}, ')
+
+
+# Getting the initial pin's position in a matrix - by flooring. I think we can use both latest or initial pin since up to this point they are equal
+x0, y0 = np.floor(pins_coordinates[latest_pin]).astype(int)
+
+latest_error = 3000
+iteration_error = 2000
+
+number_of_edges = 0
+main_canvas = get_supersampled_canvas()
+
+
+class Pin_Network: 
+	def __init__(self, pins):
+		self.connections = { pin: set() for pin in range(pins)}
+
+
+	def add_connection(self, pin1, pin2): 
+		if pin2 not in self.connections[pin1] and pin1 not in self.connections[pin2]: 
+			self.connections[pin1].add(pin2)
+			self.connections[pin2].add(pin1)
+
 	
-
-	def __to_radian(self, angle): 
-		return (math.pi * angle) / 180
-
-
-	def __get_x_0(self, angle): 
-		return self.radius * math.cos(self.__to_radian(angle))
-	
-
-	def __get_y_0(self, angle): 
-		return self.radius * math.sin(self.__to_radian(angle))
+	def is_connected(self, pin1, pin2): 
+		return pin2 in self.connections[pin1] and pin1 in self.connections[pin2]
 
 
-	def __set_pins_coordinates(self): 
-		r = self.source_image.shape[0] / 2 
+pin_network = Pin_Network(pins_amount)
 
-		for i in range(0, self.pins): 
-			x = self.__get_x_0(i * self.angle_between_pins) + r
-			y = self.__get_y_0(i * self.angle_between_pins) + r
 
-			self.pins_coordinates.append((x, y))
+while True:
+	print(number_of_edges, latest_error)
+	if iteration_error < latest_error:
+		latest_error = iteration_error
 
-	def __find_unallowed_to_connect_with(self, initial_pin_number):
-		indices = []
-		
-		output = enumerate(self.output)
-		output_length = len(self.output)
+		best_pin_number = None
 
-		for idx, value in output:
-			if value == initial_pin_number:
-				if idx != output_length - 1: 
-					indices.append(self.output[idx + 1])
+		lowest_error_step_canvas = None
 
-				if output_length > 2: 
-					indices.append(self.output[idx - 1])
+		for i in range(pins_amount):
+			# find the lowest error when connecting latest_pin to i pin
+
+			# all steps work on this canvas
+			copy_of_main_canvas = main_canvas.copy()
+
+			if i != latest_pin:
+				bresenham_points = get_bresenham(i)
+
+				x_coordinates, y_coordinates = bresenham_points[:, 0], bresenham_points[:, 1]
+								
+				x_max, y_max = copy_of_main_canvas.shape 
+
+				x_coord_plus_1_mask = (x_coordinates + 1) < x_max
+				y_coord_plus_1_mask = (y_coordinates + 1) < y_max 
+
+				exact_increment = 0.1
+				near_increment = 0.05
+				diagonal_increment = 0.01
+
+				# pixel
+				copy_of_main_canvas[x_coordinates, y_coordinates] = np.minimum(1, copy_of_main_canvas[x_coordinates, y_coordinates] + 0.2)
 				
-		return indices
-	
-	# ???       i = 10
-	def __current_pin_to_connect_with_is_allowed(self, initial_pin_number, current_pin_number): 
-		""" 
-		 
-		  Итак, мы отправляем следующую точку. Смотрим, можно ли с ней связаться.
-		  Для этого мы должны найти в списке все начальные точки и посмотреть - какие у них предыдущие и следующие индексы.
-		  С этими точками нельзя связываться.
-
-		  Далее, берем и сравниваем следующую точку с предыдущей
-		   
-			 """
-		unallowed = self.__find_unallowed_to_connect_with(initial_pin_number)
-
-
-		try: 
-			unallowed.index(current_pin_number)
-		except: 
-			return True
-		
-		return False
-
-
-	def __get_average_intensity(self, initial_pin_number, current_pin_to_connect_with):
-		x0, y0 = self.pins_coordinates[initial_pin_number] 
-		x1, y1 = self.pins_coordinates[current_pin_to_connect_with]
-
-		bresenham_args = (x0, y0, x1, y1)
-		bresenham_args_floored = list(map(lambda coordinate: math.floor(coordinate), bresenham_args))
-
-		intersection_points = list(bresenham(*bresenham_args_floored))
-
-		print(intersection_points, x0, x1)
-
-		intensity = 0
-
-		for x, y in intersection_points:
-			avg_color_value_per_pixel = 0
-
-			for color_value in self.source_image[x][y]:  
-				avg_color_value_per_pixel += color_value
-
-			intensity += avg_color_value_per_pixel / 3
-
-		return intensity
-
-
-	def __pin_is_allowed_for_overflowing_offset(self, initial_pin_number, current_pin_number, offset): 
-		lower_bound = initial_pin_number - self.pins + offset
-		upper_bound = initial_pin_number - offset
-
-		return initial_pin_number + offset > self.pins and (current_pin_number > lower_bound and current_pin_number < upper_bound)
-	
-
-	def __pin_is_allowed_for_neutral_offset(self, initial_pin_number, current_pin_number, offset): 
-		lower_bound = initial_pin_number - offset
-		upper_bound = initial_pin_number + offset
-
-		return initial_pin_number - offset > 0 and (current_pin_number < lower_bound or current_pin_number > upper_bound)
-	
-
-	def __pin_is_allowed_for_underflowing_offset(self, initial_pin_number, current_pin_number, offset): 
-		lower_bound = initial_pin_number + offset
-		upper_bound = self.pins - (offset + initial_pin_number)
-
-		return current_pin_number > lower_bound and current_pin_number < upper_bound
-
-		
-	def generate(self): 
-		initial_pin_number = randint(1, 100)
-
-		self.output.append(initial_pin_number)
-
-		# should overwrite
-		with open ('./output.txt', 'a') as output: 
-			output.write(f'{initial_pin_number},')
-
-			output.close()
-
-
-		for _ in range(0, self.iterations): 
-			best_option = { 
-				'intensity': 0, 
-				'pin_number': 0
-			}
-
-			offset = 24
-
-			
-
-			# как так получается, что повторяются 0, 25 ? 
-			for j in range(
-				0, self.pins
-			): 
-				check_if_pin_is_in_bounds_args = (initial_pin_number, j, offset)
-
-				# if self.__current_pin_to_connect_with_is_allowed(initial_pin_number, j) and (self.__pin_is_allowed_for_overflowing_offset(*check_if_pin_is_in_bounds_args) or self.__pin_is_allowed_for_neutral_offset(*check_if_pin_is_in_bounds_args) or self.__pin_is_allowed_for_underflowing_offset(*check_if_pin_is_in_bounds_args)): 
-				if self.__current_pin_to_connect_with_is_allowed(initial_pin_number, j): 
-					# also need to check if number is allowed 
-					avg_intensity = self.__get_average_intensity(initial_pin_number, j)
-
-					if avg_intensity > best_option['intensity']: 
-						best_option['intensity'] = avg_intensity
-						best_option['pin_number'] = j
-
-
-			with open('./output.txt', 'a') as output: 
-				output.write(f'{best_option["pin_number"]}, ')
+				step_error = np.linalg.norm(downsample(copy_of_main_canvas) - image_matrix_normalized)
 				
-				output.close()		
+				# when found better point
+				if step_error < iteration_error and not pin_network.is_connected(i, latest_pin):
+					best_pin_number = i
+					iteration_error = step_error
 
-			initial_pin_number = best_option['pin_number']
+					# Saved the best canvas
+					lowest_error_step_canvas = copy_of_main_canvas.copy()
 
-			self.output.append(initial_pin_number)
+		pin_network.add_connection(latest_pin, best_pin_number)
 
+		latest_pin = best_pin_number
+		number_of_edges += 1
+		# update canvas
+		main_canvas = lowest_error_step_canvas.copy()
 
+		with open('output.txt', 'a') as f:
+			f.write(f'{latest_pin}, ')
 
-""" 
+	else:
+		break
 
-	1. взять промежуток - до 6000 к примеру, и там решать сколько нитей сделать, высчитывая ошибку
-	2.  Брать какой то зазор - если фотография будет лучше выглядеть - то добавить +- n нитей 
-
-"""
-
-
-			
